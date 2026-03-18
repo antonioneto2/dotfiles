ea55de94f1a92d3fe8a0782874c91afd5d96d99e
antonioneto2 <antonio.nog28@gmail.com>
2026-03-18 20:59:37 -0300

    chore: remove diretorios antigos da raiz apos reestruturacao para a pasta .config

--- cmus/lib.pl
+++
@@ -1,6 +0,0 @@
-/home/nogueira/Documents/Redes de Computadores/Aulas em áudio/Aula01-Redes.m4a
-/home/nogueira/Documents/Redes de Computadores/Aulas em áudio/Aula02-Redes.m4a
-/home/nogueira/Documents/Redes de Computadores/Aulas em áudio/Aula03-Redes.m4a
-/home/nogueira/Documents/Redes de Computadores/Aulas em áudio/Aula04-Redes.m4a
-/home/nogueira/Documents/Redes de Computadores/Aulas em áudio/Aula05-Redes.m4a
-/home/nogueira/Documents/Redes de Computadores/Aulas em áudio/Aula06-Redes.m4a
